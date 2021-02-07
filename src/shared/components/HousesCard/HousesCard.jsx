3117207deb78b21461fe714d8f58c65cde9bc463
Sami Khalaf <samifalafel>
2021-02-07 07:05:59 +0100

    houses pages working now

--- src/shared/components/HousesCard/HousesCard.jsx
+++ src/shared/components/HousesCard/HousesCard.jsx
@@ -4,14 +4,20 @@ import { Link } from 'react-router-dom';
 import '../../../index.scss';
 import './HousesCard.scss';
 
+import NotFound from '../../../assets/img/404.png';
+
 export default function HousesCard(props) {
   return (
     <div className="house-card--container">
       {props.houses.map((house, index) => (
         <figure className="house-card">
           <Link to={`./house/${house.name}`}>
+            <img
+              className="house-card--image"
+              src={house.logoURL ? house.logoURL : NotFound}
+              alt={house.name}
+            />
             <p className="house-card--title">{house.name}</p>
-            <img className="house-card--image" src={house.image} alt={house.name} />
           </Link>
         </figure>
       ))}
